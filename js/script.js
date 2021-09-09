c8bff49154b442e607ed6f2f50ecac15d7eaa10b
Nancy Borges <nabg1504@gmail.com>
2021-09-08 23:25:11 -0300

    Desafio complementario funciones

--- js/script.js
+++ js/script.js
@@ -1,40 +1,84 @@
 
 
 let precioProducto;
-const precioTotal = precioProducto => precioProducto * 1.21;
+let opcion;
+let valorBoleta= 0;
+let precioConIva;
+let producto;
+let cuotas = 0 ;
 
-let producto = prompt("Ingrese el producto que quiere comprar( // TECLADO // MOUSE // MONITOR // )").toUpperCase();
+const precioTotal = (precioProducto) => Math.round(precioProducto * 1.21);
 
-let productoElegido = (producto) => {
-    
+
+
+const boleta = () => {
+
+    do{
+        let producto = prompt("Ingrese el producto que quiere comprar( // TECLADO // MOUSE // MONITOR // )").toUpperCase();
+
         if(producto == "TECLADO"){
             console.log("El precio del teclado es 30")
             precioProducto = 30;    
-            precioTotal(precioProducto);
-            console.log("El precio final es " + precioTotal(precioProducto));
-            return producto;
+            precioConIva = precioTotal(precioProducto);
+            console.log("El precio con Iva es " + precioConIva);
+            alert("El producto que elegiste es " + producto )
+            
             
         }else if(producto == "MOUSE"){
             console.log("El precio del Mouse es 25")
             precioProducto = 25;
-            precioTotal(precioProducto);
-            console.log("El precio final es " + precioTotal(precioProducto));
-            return producto;
-        }else{
+            precioConIva = precioTotal(precioProducto);
+            console.log("El precio con Iva es " + precioConIva);
+            alert("El producto que elegiste es " + producto )
+            
+        }else if(producto == "MONITOR"){
             console.log("El precio del Monitor es 100")
             precioProducto = 100;
-            precioTotal(precioProducto);
-            console.log("El precio final es " + precioTotal(precioProducto));
-            return producto;
+            precioConIva = precioTotal(precioProducto);
+            console.log("El precio con Iva es " + precioConIva);
+            alert("El producto que elegiste es " + producto )
+            
+        }else{
+            alert("Por favor ingrese un producto de nuestra lista")
         }
+
+        valorBoleta = valorBoleta + precioConIva;
+    
+        opcion = prompt("Desea agregar otro producto?" ).toUpperCase();
+
+
+    }while(opcion != "NO")
+
+
+
+     
+    
+}
+
+const metodoPago = (valorBoleta) =>{
+    cuotas = prompt("En cuantas cuotas lo queres abonar? (1,2 o 3) ");
+
+        if(cuotas == 1){
+            alert("El total de tu compra es: $" + valorBoleta);
+        }else if (cuotas == 2){
+            alert("El total de tu compra es: $" + valorBoleta + " y te queda en 2 cuotas de: $" + valorBoleta/2);
+        }else{
+            alert("El total de tu compra es: $" + valorBoleta + " y te queda en 3 cuotas de: $" + valorBoleta/3)
+        }
+
     
 }
 
 
-productoElegido(producto);
+
+boleta(producto);
+
 precioTotal(precioProducto);
 
-alert("El producto que elegiste es " + producto )
+metodoPago(valorBoleta);
+
+
+
 
 alert("Gracias por elegir CORE uruguay")
 
